36b442ba7febea57452ab84ed2191ebaf64f2f63
Nathan Wise <nathanwise@gmail.com>
2017-10-08 17:04:37 -0400

    Initial Commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guesstResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guesstResponse);
+                return View("Thanks", guesstResponse);
+            } else
+            {
+                return View();
+            }
+       }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
